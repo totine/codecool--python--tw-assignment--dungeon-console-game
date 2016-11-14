c4eed2315f40690f185f1788dee8807842e42490
Marek Frankowicz <frankowicz.marek@gmail.com>
2016-11-14 15:17:12 +0100

    Movemnt after some cleaning

--- game.py
+++ game.py
@@ -1,9 +1,13 @@
 import sys
 import os
 
+
 os.system("printf '\033c'")
 sys.stdout.write("\x1b[8;{rows};{cols}t".format(rows=30, cols=110))
 PLAYER = "@"
+position_x = 1
+position_y = 1
+
 
 def tablica(x, y):
     lista = []
@@ -22,39 +26,29 @@ def drukowanie_tablicy(lista):
         print(''.join(i).rjust(105, " "))
 
 
-plansza = tablica(25,86)
-plansza[5][1] = "@"
-position_x = 1
-position_y = 1
-
-
-def player_position(lista,pos1,pos2):
+def player_position(lista, pos1, pos2):
     x = [(index, row.index("@")) for index, row in enumerate(lista) if "@" in row]
-    print(x)
-    print(x[0][0])
-    lista [pos1][pos2] = "@"
-    lista [x[0][0]][x[0][1]] = "."
+    lista[pos1][pos2] = "@"
+    lista[x[0][0]][x[0][1]] = "."
     return lista
 
-plansza = player_position(plansza,position_x,position_y)
+
+def getch():
+    import sys
+    import tty
+    import termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
 
 
-drukowanie_tablicy(plansza)
-
-while 1:
-    def getch():
-        import sys, tty, termios
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
-
-    x = getch()
-
+def user_input(x):
+    global position_x, position_y
     if x == "w":
         position_x = position_x - 1
         position_y = position_y
@@ -69,8 +63,14 @@ while 1:
         position_y = position_y + 1
 
 
+plansza = tablica(25, 86)
+plansza[1][1] = "@"
+drukowanie_tablicy(plansza)
 
-    plansza = player_position(plansza,position_x,position_y)
 
+while 1:
+    x = getch()
+    user_input(x)
+    plansza = player_position(plansza, position_x, position_y)
     os.system("printf '\033c'")
     drukowanie_tablicy(plansza)
