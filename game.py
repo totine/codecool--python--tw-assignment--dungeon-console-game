ebe2fc298e2e8be75efe1e69315bb00313e8d42b
Marek Frankowicz <frankowicz.marek@gmail.com>
2016-11-14 13:17:09 +0100

    poczatek

---
+++ game.py
@@ -0,0 +1,25 @@
+import sys
+import os
+
+os.system('clear')
+sys.stdout.write("\x1b[8;{rows};{cols}t".format(rows=30, cols=110))
+
+
+def tablica(x, y):
+    lista = []
+    for rzad in range(x):
+        lista.append([])
+        for kolumn in range(y):
+            if rzad == 0 or rzad == x-1 or kolumn == 0 or kolumn == y-1:
+                lista[rzad].append('x')
+            else:
+                lista[rzad].append('.')
+    return lista
+
+
+def drukowanie_tablicy(lista):
+    for i in lista:
+        print(''.join(i).rjust(105, " "))
+
+
+drukowanie_tablicy(tablica(25,86))
